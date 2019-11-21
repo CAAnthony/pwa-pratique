98d2f8e3a6a4da42badd3dd2868cf95f92a8750d
CAAnthony <anthony.careddu23@gmail.com>
2019-11-21 10:18:37 +0100

    3. Service worker installé

--- main.js
+++ main.js
@@ -16,3 +16,13 @@ function loadTechnologies(technos) {
 }
 
 loadTechnologies(technos);
+
+if(navigator.serviceWorker) {
+	
+    navigator.serviceWorker
+	
+        .register('sw.js')
+	
+        .catch(err => console.error('service worker NON enregistré', err));
+	
+}
